42f26f9c58363dcbf029587f938092242f1e1153
anis eljeljal <anis.eljeljal@epfedu.fr>
2023-09-25 11:15:20 +0200

    AJOUT EXO1

---
+++ TP0_ELJELJAL/EXO 1/src/exo/pkg1/EXO1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exo.pkg1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author anis_
+ */
+public class EXO1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+// TODO code application logic here
+                Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prenom ?");
+        sc.nextLine();
+
+    }
+    
+}
