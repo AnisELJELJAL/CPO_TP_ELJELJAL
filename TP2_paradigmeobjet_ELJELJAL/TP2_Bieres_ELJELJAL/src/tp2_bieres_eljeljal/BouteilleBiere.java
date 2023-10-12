c9b5b97b803add3935b74f08421c7baf77196b8c
anis eljeljal <anis.eljeljal@epfedu.fr>
2023-10-12 23:17:41 +0200

    AJOUT TP2_biere_ELJELJAL

--- TP2_paradigmeobjet_ELJELJAL/TP2_Bieres_ELJELJAL/src/tp2_bieres_eljeljal/BouteilleBiere.java
+++ TP2_paradigmeobjet_ELJELJAL/TP2_Bieres_ELJELJAL/src/tp2_bieres_eljeljal/BouteilleBiere.java
@@ -20,7 +20,30 @@ public class BouteilleBiere {
         degreAlcool= inDegreAlcool;
         brasserie= inBrasserie;
     }
-    
+    public boolean decapsuler ()
+    {
+        if (ouverte != false) {
+            ouverte = true;
+            return true;
+        }
+        else {
+            System.out.println("Bouteille déjà ouverte.");
+            return false;
+        }
+    }
+    @Override
+    public String toString() {
+        String chaine_a_retourner;
+        chaine_a_retourner = nom + " (" + degreAlcool + " degrés) Ouverte ? ";
+        if (ouverte == true ) {
+            chaine_a_retourner += "oui";
+        }
+        else {
+            chaine_a_retourner += "non";
+        }
+        return chaine_a_retourner ;
+    }
+
     public void lireEtiquette() {
 System.out.println("Bouteille de " + nom +" (" + degreAlcool +
  " degres) \nBrasserie : " + brasserie ) ;
