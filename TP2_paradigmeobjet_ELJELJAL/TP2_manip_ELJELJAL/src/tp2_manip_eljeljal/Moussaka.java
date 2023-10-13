bf47ce5a8a5b068bdb291d76081cf05376a48f65
anis eljeljal <anis.eljeljal@epfedu.fr>
2023-10-13 14:53:15 +0200

    AJOUT TP2_manip_ELJELJAL

---
+++ TP2_paradigmeobjet_ELJELJAL/TP2_manip_ELJELJAL/src/tp2_manip_eljeljal/Moussaka.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_eljeljal;
+
+/**
+ *
+ * @author anis_
+ */
+public class Moussaka {
+    int nbCalories;
+  
+   public Moussaka(int calories) {
+       nbCalories = calories;
+
+ }
+   public int nbCalories() {
+    return nbCalories;
+}
+   
+}
