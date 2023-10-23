55a2cba0be6a6b8fe5e3b3cef58171b34070a835
anis eljeljal <anis.eljeljal@epfedu.fr>
2023-10-23 15:21:28 +0200

    AJOUT TP3, ajout des armes baton epee ainsi que debut creation personnage

--- TP3_Heroic_Fantasy_ELJELJAL/src/tp3_heroic_fantasy_eljeljal/TP3_Heroic_Fantasy_ELJELJAL.java
+++ TP3_Heroic_Fantasy_ELJELJAL/src/tp3_heroic_fantasy_eljeljal/TP3_Heroic_Fantasy_ELJELJAL.java
@@ -4,6 +4,9 @@
  */
 package tp3_heroic_fantasy_eljeljal;
 
+import Armes.Epee;
+import Armes.Baton;
+
 /**
  *
  * @author anis_
@@ -15,6 +18,21 @@ public class TP3_Heroic_Fantasy_ELJELJAL {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Epee epee1 = new Epee ("Excalibur", 7, 5);
+        Epee epee2 = new Epee ("Durandal", 4, 7);
+        
+        Baton baton1 = new Baton("Chêne",4,5);
+        Baton baton2 = new Baton ("Charme", 5, 6);
+        
+        ArrayList<Armes> tabArme = new ArrayList<Armes>();
+        tabArme.add(epee1);
+        tabArme.add(epee2);
+        tabArme.add(baton1);
+        tabArme.add(baton2);
+         for (int i=0; i<4;i++){
+            System.out.print(tabArme.get(i));
+        }
+        System.out.println();
     }
     
 }
